b80b5db634e58f4ac41a18192f77e00eb54a6916
Sjo31-12 <A00832366@itesm.mx>
2023-04-16 13:27:37 -0600

    2 Vistas, 3 componentes
    Progreso de la vista de CrossFit, Esports.
    Creación de 3 componentes: Header, Footer y Calendar (en proceso)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EsportsComponent } from './esports/esports.component';
+import { CrossfitComponent } from './crossfit/crossfit.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EsportsComponent,
+    CrossfitComponent,
+    HeaderComponent,
+    FooterComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule
