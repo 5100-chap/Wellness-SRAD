d4af722a2bf2a8e33825a39b8cb3f554d2dc5a2f
Diego Alberto Baños Lopez <A01275100@tec.mx>
2023-04-20 23:35:00 -0600

    Modificacion de headers respecto al login y el resto de las paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { NgClass } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'prueba1';
- 
+  title = 'Wellness-SRAD';
+
+  router: string;
+
+  constructor(public _router: Router) {
+    this.router = _router.url;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,7 +58,6 @@ const router: Routes = [
   {
     path: 'gimnasio',
     component: GimnasioComponent
- 
   }
 
 ];
