0323e08ea69a8d44e03c1933395464cc5441d2b9
5100-chap <A01275100@tec.mx>
2023-05-16 10:40:29 -0600

    Corregido bug de Login y modificado comentarios en RoleRoutes

--- API/config/routes/RoleRoutes.js
+++ API/config/routes/RoleRoutes.js
@@ -22,36 +22,36 @@ router.get("/api/getAllAlumni", async (req, res, next) => {
 
 router.get("/api/getXCredentials", async (req, res, next) => {
     try {
-        // Get the authorization header
+        // Consigue el header de autentificacion
         const authHeader = req.headers.authorization;
-        
-        // Check if the header is present and contains Basic authentication
+
+        // Checa si el header tiene autentificación basica
         if (!authHeader || !authHeader.startsWith('Basic ')) {
             res.status(401).send('Unauthorized');
             return;
         }
-        
-        // Decode the base64-encoded credentials
+
+        // Decodifica las crendenciales
         const encodedCredentials = authHeader.slice('Basic '.length);
         const decodedCredentials = Buffer.from(encodedCredentials, 'base64').toString();
-        
-        // Parse the credentials into username and password
+
+        // Almacena el usuario y contraseña
         const [username, password] = decodedCredentials.split(':');
         // Creacion de variables de query para buscar
         var result;
         const roles = [
-            { role: 'Alumno', IdForm:"Matricula", param: 'matricula', type: sql.Char(9) },
-            { role: 'Director', IdForm:"Nomina", param: 'numero_nomina', type: sql.VarChar(30) },
-            { role: 'Administrador', IdForm:"Nomina", param: 'numero_nomina', type: sql.VarChar(30) },
-            { role: 'Instructor', IdForm:"Nomina", param: 'numero_nomina', type: sql.VarChar(30) },
+            { role: 'Alumno', IdForm: "Matricula", param: 'matricula', type: sql.Char(9) },
+            { role: 'Director', IdForm: "Nomina", param: 'numero_nomina', type: sql.VarChar(30) },
+            { role: 'Administrador', IdForm: "Nomina", param: 'numero_nomina', type: sql.VarChar(30) },
+            { role: 'Instructor', IdForm: "Nomina", param: 'numero_nomina', type: sql.VarChar(30) },
         ];
-        
+
         for (const { role, param, type, IdForm } of roles) {
             const request = new sql.Request();
             request.input(param, type, username);
-        
+
             result = await request.execute(`Get${role}By${IdForm}`);
-            
+
             if (result.recordset.length === 1) {
                 result.recordset[0].role = role;
                 if (result.recordset[0].contrasena !== password) {
@@ -63,14 +63,14 @@ router.get("/api/getXCredentials", async (req, res, next) => {
                 break;
             }
         }
-        
+
         if (result.recordset.length !== 1) {
             console.log("Triggered illegal instruction");
             result.recordset = null;
             res.status(401).send('Unauthorized');
             return;
         }
-        
+
         res.send(result.recordset);
     } catch (err) {
         next(err);
