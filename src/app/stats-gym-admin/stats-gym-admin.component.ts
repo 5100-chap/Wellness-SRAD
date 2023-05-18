48f16c792b1acadc17ef4d0bf875569e1532ac22
Diego Alberto Baños Lopez <A01275100@tec.mx>
2023-05-17 23:01:06 -0600

    Correcciones en la implementacion de Aforo por semana en el GYM

--- src/app/stats-gym-admin/stats-gym-admin.component.ts
+++ src/app/stats-gym-admin/stats-gym-admin.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ApiService } from '../api.service';
-import Chart, { Legend, plugins } from 'chart.js/auto';
+import Chart from 'chart.js/auto';
+import { Subscription } from 'rxjs';
 
 interface AforoSemanalResponse {
   DayOfWeek: number;
@@ -12,12 +14,19 @@ interface AforoSemanalResponse {
   templateUrl: './stats-gym-admin.component.html',
   styleUrls: ['./stats-gym-admin.component.css'],
 })
-export class StatsGymAdminComponent implements OnInit {
+export class StatsGymAdminComponent implements OnInit, OnDestroy {
   public chart: any;
+  dateControl = new FormControl();
+  private subscription: Subscription | undefined;
 
   constructor(private apiService: ApiService) {}
 
   createChart(labels: string[], data: number[]) {
+    // Si ya existe un gráfico, lo destruimos
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
     this.chart = new Chart('Dia', {
       type: 'bar',
 
@@ -38,7 +47,6 @@ export class StatsGymAdminComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const date = '2023-05-17';
     const areaId = 52;
 
     // Definir los nombres de los días de la semana
@@ -52,14 +60,51 @@ export class StatsGymAdminComponent implements OnInit {
       'Domingo',
     ];
 
-    this.apiService
-      .getAforoSemanal(date, areaId)
-      .subscribe((data: AforoSemanalResponse[]) => {
-        // Convertir de número del día de la semana a nombre del día
-        const labels = data.map((item) => daysOfWeek[item.DayOfWeek - 1]);
-        const dataPoints = data.map((item) => item.AttendanceCount);
+    this.subscription = this.dateControl.valueChanges.subscribe(
+      (value: string) => {
+        const year = Number(value.slice(0, 4));
+        const week = Number(value.slice(-2));
 
-        this.createChart(labels, dataPoints);
-      });
+        const date = new Date(year, 0, 1 + (week - 1) * 7);
+        const dayOfWeek = date.getDay();
+        const ISOweekStart = date;
+        if (dayOfWeek <= 4)
+          ISOweekStart.setDate(date.getDate() - date.getDay() + 1);
+        else ISOweekStart.setDate(date.getDate() + 8 - date.getDay());
+
+        const formattedDate = ISOweekStart.toISOString().split('T')[0];
+
+        this.apiService
+          .getAforoSemanal(formattedDate, areaId)
+          .subscribe((data: AforoSemanalResponse[]) => {
+            // Inicializar un objeto con todos los días de la semana y aforo 0
+            const attendanceByDay: { [day: string]: number } = {
+              Lunes: 0,
+              Martes: 0,
+              Miércoles: 0,
+              Jueves: 0,
+              Viernes: 0,
+              Sábado: 0,
+              Domingo: 0,
+            };
+
+            // Llenar el objeto con los datos obtenidos
+            for (const item of data) {
+              attendanceByDay[daysOfWeek[item.DayOfWeek - 1]] =
+                item.AttendanceCount;
+            }
+
+            // Crear los arreglos labels y dataPoints a partir del objeto
+            const labels = Object.keys(attendanceByDay);
+            const dataPoints = Object.values(attendanceByDay);
+
+            this.createChart(labels, dataPoints);
+          });
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
