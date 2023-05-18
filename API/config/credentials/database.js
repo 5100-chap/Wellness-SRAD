dc21ca4bdd6771a71f5a72c8c0999792bf68fdb2
5100-chap <A01275100@tec.mx>
2023-05-18 12:43:13 -0600

    Cambios en archivos de credenciales

--- API/config/credentials/database.js
+++ API/config/credentials/database.js
@@ -1,19 +1,19 @@
 //const sql = require('mssql');
 require('dotenv').config();
 
-//Credenciales extraidas del archivo .env
+//Credenciales extraidas del archivo .env utilizadas en Azure
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_DATABASE,
   options : {
-    trustedConnection: true,
     encrypt: true,
     enableArithAbort: true,
     trustServerCertificate: true,
   }
 };
+
 
 module.exports = {
   config, //Esta linea devuelve las credenciales
