b5ec3ac00a4a434ea69d1e4ae83abcbdf54b9dc3
Marcelo91216 <75495866+Marcelo91216@users.noreply.github.com>
2023-06-01 12:44:23 -0600

    v6.0

--- src/app/lista-reservas/lista-reservas.component.ts
+++ src/app/lista-reservas/lista-reservas.component.ts
@@ -68,7 +68,6 @@ export class ListaReservasComponent {
         }
         this.slices.push(data.length);
       }
-      console.log(this.slices);
     },
     error=>{
       console.error('Error fetching all reservas from alumno --> ', error);
@@ -78,24 +77,18 @@ export class ListaReservasComponent {
   cancelarReserva(index: number){
     const usuario = this.authService.currentUserValue['username'];
     this.apiService.cancelarReservaAlumno(usuario, this.Reservas[index]['id']).subscribe(()=>{
-    },error=>{
-      console.log(error);
     });
   }
 
   marcarLlegada(index: number){
     const usuario = this.authService.currentUserValue['username'];
     this.apiService.marcarLlegadaReserva(usuario, this.Reservas[index]['id_area_deportiva'], this.Reservas[index]['id']).subscribe(()=>{
-    },error=>{
-      console.log(error);
     });
   }
 
   marcarSalida(index: number){
     const usuario = this.authService.currentUserValue['username'];
     this.apiService.marcarSalidaReserva(usuario, this.Reservas[index]['id']).subscribe(()=>{
-    }, error=>{
-      console.log(error);
     });
   }
   
@@ -114,10 +107,6 @@ export class ListaReservasComponent {
     const TodosLosDias = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado', 'Domingo'];
     const diaSemana = TodosLosDias[dia.getDay()];
     return `${diaSemana} ${dato.slice(8, 10)} de ${nombreMes}`;
-  }
-
-  printIndex(index: number){
-    console.log(index);
   }
 
   marcarLlegadaBtn(index: number): boolean{
